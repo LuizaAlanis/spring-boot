6b51f09dc3c12cb5937ee62dfbfed2f685084170
luizaalanis <luizalanisantos@gmail.com>
2022-11-11 13:01:50 -0300

    New endpoint

---
+++ src/main/java/com/luiza/estudos/service/Product.java
@@ -0,0 +1,13 @@
+package com.luiza.estudos.service;
+
+public class Product {
+    private String name;
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+}
--- src/main/java/com/luiza/estudos/service/Service.java
+++ src/main/java/com/luiza/estudos/service/Service.java
@@ -1,6 +1,16 @@
 package com.luiza.estudos.service;
+import com.sun.org.apache.bcel.internal.generic.RETURN;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping()
+@RestController
+@RequestMapping("/service")
 public class Service {
+
+     @RequestMapping("/product")
+     public String product(){
+          Product product = new Product();
+          product.setName("potatoes");
+          return product.getName();
+     }
 }
